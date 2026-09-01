18941f4fa0b522e994eb4d7a13eb40f72084e362
Thanhnhan2848 <thanhnhan2848@gmail.com>
2026-09-02 01:10:58 +0700

    Update app.py

--- app.py
+++ app.py
@@ -87,7 +87,7 @@ def home():
     </head>
     <body>
         <div class="box sub">
-            <a href="/frp_info.config">https://freedatavina.onrender.com/frp_info.config</a>
+            <a href="/frp_info.config">/frp_info.config</a>
         </div>
 
         <div class="box">
